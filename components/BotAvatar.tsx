3586b7c05d4d620cd3efa973e0b1e88daa1b09ab
Oliver <oliver.lewandowski1@gmail.com>
2023-08-14 23:10:59 +0200

    typo fix

--- components/BotAvatar.tsx
+++ components/BotAvatar.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarImage } from '@/components/ui/Avatar';
 
 export const BotAvatar = () => (
   <Avatar className='w-8 h-8'>
